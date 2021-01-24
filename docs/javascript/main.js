f6165847eb865deb22b5406bd0a0a10af8f51178
Xinran Yu <66088463+xinrany@users.noreply.github.com>
2021-01-24 16:17:52 -0600

    Add back to top button, update teaching page and CV
    Add back to top button. Update teaching page (spring 2021) and CV

--- docs/javascript/main.js
+++ docs/javascript/main.js
@@ -29,3 +29,62 @@ accordionItemHeaders.forEach((accordionItemHeader) => {
     }
   });
 });
+
+//back to top button
+const backToTopButton = document.querySelector("#back-to-top-btn");
+
+window.addEventListener("scroll", scrollFuction);
+
+function scrollFuction() {
+  if (window.pageYOffset > 100) {
+    //Show backToTopButton
+    if (!backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnExit");
+      backToTopButton.classList.add("btnEntrance");
+      backToTopButton.style.display = "block";
+    }
+  } else {
+    //Hide backToTopButton
+    if (backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnEntrance");
+      backToTopButton.classList.add("btnExit");
+      setTimeout(function () {
+        backToTopButton.style.display = "none";
+      }, 250);
+    }
+  }
+}
+
+backToTopButton.addEventListener("click", backToTop);
+
+function backToTop() {
+  window.scrollTo(0, 0);
+}
+
+// //smooth scrolling
+// function smoothScrollBackToTop() {
+//   const targetPosition = 0;
+//   const startPosition = window.pageYOffset;
+//   const distance = targetPosition - startPosition;
+//   const duration = 750;
+//   let start = null;
+
+//   window.requestAnimationFrame(step);
+
+//   function step(timestamp) {
+//     if (!start) start = timestamp;
+//     const progress = timestamp - start;
+//     window.scrollTo(
+//       0,
+//       easeInOutCubic(progress, startPosition, distance, duration)
+//     );
+//     if (progress < duration) window.requestAnimationFrame(step);
+//   }
+// }
+
+// fuction easeInOutCubic(t, b, c, d){
+//   t /= d/2;
+//   if (t < 1) return c/2*t*t*t + b;
+//   t -= 2;
+//   return c/2*(t*t*t + 2) + b;
+// };
