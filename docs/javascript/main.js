20335e00b5d5b72de10a501fca07482f5a712f63
Xinran Yu <66088463+xinrany@users.noreply.github.com>
2024-03-23 19:40:47 -0500

    Update home page

--- docs/javascript/main.js
+++ docs/javascript/main.js
@@ -60,31 +60,3 @@ backToTopButton.addEventListener("click", backToTop);
 function backToTop() {
   window.scrollTo(0, 0);
 }
-
-// //smooth scrolling
-// function smoothScrollBackToTop() {
-//   const targetPosition = 0;
-//   const startPosition = window.pageYOffset;
-//   const distance = targetPosition - startPosition;
-//   const duration = 750;
-//   let start = null;
-
-//   window.requestAnimationFrame(step);
-
-//   function step(timestamp) {
-//     if (!start) start = timestamp;
-//     const progress = timestamp - start;
-//     window.scrollTo(
-//       0,
-//       easeInOutCubic(progress, startPosition, distance, duration)
-//     );
-//     if (progress < duration) window.requestAnimationFrame(step);
-//   }
-// }
-
-// fuction easeInOutCubic(t, b, c, d){
-//   t /= d/2;
-//   if (t < 1) return c/2*t*t*t + b;
-//   t -= 2;
-//   return c/2*(t*t*t + 2) + b;
-// };
